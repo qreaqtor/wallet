101c7aeed41ac2fc9da55578ecbdad3d5a9d0e73
qreaqtor <qreaqtor@gmail.com>
2025-11-06 02:32:30 +0500

    main

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -6,10 +6,15 @@ import (
 	"os/signal"
 	"syscall"
 
-	getwallet "github.com/qreaqtor/wallet/internal/api/handler/get_wallet"
-	updatewallet "github.com/qreaqtor/wallet/internal/api/handler/update_wallet"
-	"github.com/qreaqtor/wallet/internal/config"
-	"github.com/qreaqtor/wallet/pkg/api/handler"
+	errwrapper "github.com/qreaqtor/wallet/internal/api/error_wrapper"
+	getwallet_h "github.com/qreaqtor/wallet/internal/api/handler/get_wallet"
+	swaggerhandler "github.com/qreaqtor/wallet/internal/api/handler/swagger"
+	updatewallet_h "github.com/qreaqtor/wallet/internal/api/handler/update_wallet"
+	ratelimiter "github.com/qreaqtor/wallet/internal/api/rate_limiter"
+	wallet_cache "github.com/qreaqtor/wallet/internal/infrastucture/cache/wallet"
+	"github.com/qreaqtor/wallet/internal/infrastucture/di"
+	wallet_repo "github.com/qreaqtor/wallet/internal/infrastucture/repository/wallet"
+	updatewallet_uc "github.com/qreaqtor/wallet/internal/usecase/update_wallet"
 	"github.com/qreaqtor/wallet/pkg/api/server"
 	"github.com/qreaqtor/wallet/pkg/logger"
 )
@@ -24,18 +29,45 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	cfg, err := config.Load()
+	cfg, err := di.LoadConfig()
+	if err != nil {
+		return err
+	}
+
+	db, err := di.NewDB(ctx, cfg.Database)
 	if err != nil {
 		return err
 	}
 
 	log := logger.New(cfg.Log.Level, cfg.Log.Pretty)
 
-	apiServer := server.New(log, 8080)
+	limiter := ratelimiter.New(cfg.LimiterRPS)
 
-	apiServer.Handle(getwallet.Method, getwallet.Path, handler.New(log, getwallet.New()))
+	walletRepo := wallet_repo.New(db)
 
-	apiServer.Handle(updatewallet.Method, updatewallet.Path, handler.New(log, updatewallet.New()))
+	walletCache := wallet_cache.New(walletRepo)
+
+	updateUC := updatewallet_uc.New(walletCache)
+
+	apiServer := server.New(log, cfg.Port)
+
+	apiServer.Handle(
+		swaggerhandler.Method,
+		swaggerhandler.Path,
+		errwrapper.New(log, swaggerhandler.New()),
+	)
+
+	apiServer.Handle(
+		getwallet_h.Method,
+		getwallet_h.Path,
+		errwrapper.New(log, getwallet_h.New(limiter, walletCache)),
+	)
+
+	apiServer.Handle(
+		updatewallet_h.Method,
+		updatewallet_h.Path,
+		errwrapper.New(log, updatewallet_h.New(limiter, updateUC)),
+	)
 
 	return apiServer.Run(ctx)
 }
