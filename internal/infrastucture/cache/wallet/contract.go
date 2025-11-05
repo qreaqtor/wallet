a9b1044832b448d17cc0d4c5bda9389acfc69608
qreaqtor <qreaqtor@gmail.com>
2025-11-06 02:30:30 +0500

    cache

---
+++ internal/infrastucture/cache/wallet/contract.go
@@ -0,0 +1,15 @@
+package wallet
+
+import (
+	"context"
+
+	"github.com/qreaqtor/wallet/internal/domain/entity"
+	"github.com/qreaqtor/wallet/internal/domain/types"
+)
+
+//go:generate mockgen -source=contract.go -destination=mock/contract.go -package=mock_cache
+
+type repo interface {
+	GetByID(ctx context.Context, walletID types.WalletID) (entity.Wallet, error)
+	Upsert(ctx context.Context, walletID types.WalletID, balance int64) (entity.Wallet, error)
+}
